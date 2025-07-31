cdf18e75b04133062aeb449ab851ce62be4de18e
Boris-OC <jekovboris@gmail.com>
2025-07-31 17:23:44 +0200

    oui

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mon-portfolio/', // ← remplace par le nom de ton repo GitHub
+  base: '/portfolio-vercel/', // ← remplace par le nom de ton repo GitHub
   plugins: [react()],
 });
