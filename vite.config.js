8a229b7b42d3d550bfa1447fe2c6a76e81609984
Boris-OC <jekovboris@gmail.com>
2025-07-31 17:30:13 +0200

    Fix: configuration propre pour Vercel (plus de base)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-vercel/', // ← remplace par le nom de ton repo GitHub
   plugins: [react()],
 });
